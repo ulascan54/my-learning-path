b2161d54bcc9132ad279561b1088114a826e193d
ulascan54 <ulascandemirbag@gmail.com>
2021-09-14 16:35:27 +0300

    side bar oluşturuldu

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,9 +1,14 @@
+
+
 module.exports = {
   mode:'jit',
   purge: ['./src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors:{
+        main:'#32445a'
+      },
       spacing:{
         '15':'3.75rem'
       }
@@ -12,5 +17,20 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities, theme, e }) {
+      const calcUtilities = _.map(theme('spacing'), (value, key) => {
+        return {
+          [`.${e(`calc-h-full-${key}`)}`]: {
+            transform: `calc(100% - ${value})`
+          },
+          [`.${e(`calc-w-full-${key}`)}`]: {
+            transform: `calc(100% - ${value})`
+          }
+        }
+      })
+
+      addUtilities(calcUtilities)
+    })
+  ],
 }
