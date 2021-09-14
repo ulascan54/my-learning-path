aa6fa0fc6f8101fe205d67af0d08a01c7befc9dc
ulascan54 <ulascandemirbag@gmail.com>
2021-09-14 12:20:34 +0300

    tailwid setup

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./src/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        '15':'3.75rem'
+      }
+    },
   },
   variants: {
     extend: {},
