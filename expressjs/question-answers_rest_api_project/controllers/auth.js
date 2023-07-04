5888f48881795aa61b3e6a194acd01c7d8d63320
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-04 20:06:38 +0300

    logout functionality

--- expressjs/question-answers_rest_api_project/controllers/auth.js
+++ expressjs/question-answers_rest_api_project/controllers/auth.js
@@ -2,7 +2,10 @@ const User = require('../models/User');
 const CustomError = require('../helpers/error/CustomError');
 const asyncErrorWrapper = require('express-async-handler');
 const { sendJwtToClient } = require('../helpers/authorization/tokenHelpers');
-const { validateUserInput,comparePassword} = require('../helpers/input/inputHelpers')
+const {
+  validateUserInput,
+  comparePassword,
+} = require('../helpers/input/inputHelpers');
 
 const register = asyncErrorWrapper(async (req, res, next) => {
   //post data
@@ -18,20 +21,33 @@ const register = asyncErrorWrapper(async (req, res, next) => {
 });
 
 const login = asyncErrorWrapper(async (req, res, next) => {
-  const {email,password}= req.body
-  if(!validateUserInput(email,password)){
-    return next(new CustomError('Please Check Your Inputs',400))
+  const { email, password } = req.body;
+  if (!validateUserInput(email, password)) {
+    return next(new CustomError('Please Check Your Inputs', 400));
   }
-  
-  const user= await User.findOne({email}).select("+password")
 
-  if(!comparePassword(password,user.password)){
-    return next(new CustomError('Please Check Your credentials',400))
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!comparePassword(password, user.password)) {
+    return next(new CustomError('Please Check Your credentials', 400));
   }
 
   sendJwtToClient(user, res);
-})
+});
 
+const logout = asyncErrorWrapper(async (req, res, next) => {
+  const { NODE_ENV } = process.env;
+
+  return res.status(200)
+  .cookie({
+    httpOnly:true,
+    expires:new Date(Date.now()),
+    secure:NODE_ENV === 'development' ? false : true
+  }).json({
+    success:true,
+    message:'logout successful'
+  })
+});
 
 const getUser = (req, res, next) => {
   res.json({
@@ -46,5 +62,6 @@ const getUser = (req, res, next) => {
 module.exports = {
   register,
   login,
+  logout,
   getUser,
 };
--- expressjs/question-answers_rest_api_project/routers/auth.js
+++ expressjs/question-answers_rest_api_project/routers/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register,login, getUser } = require('../controllers/auth');
+const { register, login, getUser, logout } = require('../controllers/auth');
 const { getAccessToRoute } = require('../middlewares/authorization/auth');
 
 //api/auth
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/profile', getAccessToRoute, getUser);
+router.get('/logout', getAccessToRoute, logout);
 
 module.exports = router;
