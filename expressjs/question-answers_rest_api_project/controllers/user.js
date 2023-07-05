329ea3254797b857c98e6dc5cd7fc1841bdfb79d
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-05 22:13:08 +0300

    getAllUsers middleware function added

--- expressjs/question-answers_rest_api_project/controllers/user.js
+++ expressjs/question-answers_rest_api_project/controllers/user.js
@@ -12,6 +12,16 @@ const getSingleUser = asyncErrorWrapper(async (req, res, next) => {
     });
 });
 
+const getAllUsers = asyncErrorWrapper(async (req, res, next) => {
+    const users= await User.find()
+
+    return res.status(200).json({
+        success:true,
+        data:{users}
+    })
+});
+
 module.exports = {
     getSingleUser,
+    getAllUsers
 };
--- expressjs/question-answers_rest_api_project/routers/user.js
+++ expressjs/question-answers_rest_api_project/routers/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getSingleUser } = require('../controllers/user');
+const { getSingleUser,getAllUsers } = require('../controllers/user');
 const router = express.Router();
 const {
     checkUserExist,
 } = require('../middlewares/database/databaseErrorHelpers');
 router.get('/:id', checkUserExist, getSingleUser);
+router.get('/', getAllUsers);
 
 module.exports = router;
