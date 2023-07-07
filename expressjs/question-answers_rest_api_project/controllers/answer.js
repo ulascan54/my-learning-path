97aece9c7e3bd5450eab8041914865da58ac20e6
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-07 12:38:58 +0300

    get all answers by questions

--- expressjs/question-answers_rest_api_project/controllers/answer.js
+++ expressjs/question-answers_rest_api_project/controllers/answer.js
@@ -18,4 +18,17 @@ const addNewAnswerToQuestion = asyncErrorWrapper(async (req, res, next) => {
     });
 });
 
-module.exports = { addNewAnswerToQuestion };
+const getAllAnswersByQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { question_id } = req.params;
+    const question = await Question.findById(question_id).populate('answers');
+
+    const answers = question.answers;
+
+    return res.status(200).json({
+        success: true,
+        count: answers.length,
+        data: answers,
+    });
+});
+
+module.exports = { addNewAnswerToQuestion, getAllAnswersByQuestion };
--- expressjs/question-answers_rest_api_project/routers/answer.js
+++ expressjs/question-answers_rest_api_project/routers/answer.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { getAccessToRoute } = require('../middlewares/authorization/auth');
-const { addNewAnswerToQuestion } = require('../controllers/answer');
+const {
+    addNewAnswerToQuestion,
+    getAllAnswersByQuestion,
+} = require('../controllers/answer');
 
 const router = express.Router({ mergeParams: true });
 
 router.post('/', getAccessToRoute, addNewAnswerToQuestion);
+router.get('/', getAllAnswersByQuestion);
 
 module.exports = router;
