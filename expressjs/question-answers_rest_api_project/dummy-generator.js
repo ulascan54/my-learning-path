9d1557939c0070dd70e6f4974ca2c99ba2014d26
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-07 15:01:27 +0300

    filtered questions

--- expressjs/question-answers_rest_api_project/dummy-generator.js
+++ expressjs/question-answers_rest_api_project/dummy-generator.js
@@ -1,64 +1,54 @@
-const Question = require("./models/Question");
-const Answer = require("./models/Answer");
-const User = require("./models/User");
-const fs = require("fs");
-const connectDatabase = require("./helpers/database/connectDatabase");
-const CustomError = require("./helpers/error/customError");
+const Question = require('./models/Question');
+const Answer = require('./models/Answer');
+const User = require('./models/User');
+const fs = require('fs');
+const connectDatabase = require('./helpers/database/connectDatabase');
+const CustomError = require('./helpers/error/customError');
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-const path = "./dummy/";
+const path = './dummy/';
 
-const users = JSON.parse(fs.readFileSync(path + "users.json" ));
-const questions = JSON.parse(fs.readFileSync(path + "questions.json" ));
-const answers = JSON.parse(fs.readFileSync(path + "answers.json" ));
-
-
+const users = JSON.parse(fs.readFileSync(path + 'users.json'));
+const questions = JSON.parse(fs.readFileSync(path + 'questions.json'));
+const answers = JSON.parse(fs.readFileSync(path + 'answers.json'));
 
 dotenv.config({
-    path : "./config/.env"
+    path: './config/.env',
 });
 
 connectDatabase();
 
-const importAllData = async function(){
+const importAllData = async function () {
     try {
         await User.create(users);
         await Question.create(questions);
         await Answer.create(answers);
-        console.log("Import Process Successful");
-
-    }   
-    catch(err) {
+        console.log('Import Process Successful');
+    } catch (err) {
         console.log(err);
-        console.err("There is a problem with import process");
-    }
-    finally {
+        console.err('There is a problem with import process');
+    } finally {
         process.exit();
     }
 };
 
-const deleteAllData = async function(){
+const deleteAllData = async function () {
     try {
         await User.deleteMany();
         await Question.deleteMany();
         await Answer.deleteMany();
-        console.log("Delete Process Successful");
-
-
-    }   
-    catch(err) {
+        console.log('Delete Process Successful');
+    } catch (err) {
         console.log(err);
-        console.err("There is a problem with delete process");
-    }
-    finally {
+        console.err('There is a problem with delete process');
+    } finally {
         process.exit();
     }
 };
 
-if (process.argv[2] == "--import"){
+if (process.argv[2] == '--import') {
     importAllData();
-}
-else if (process.argv[2] == "--delete"){
+} else if (process.argv[2] == '--delete') {
     deleteAllData();
 }
