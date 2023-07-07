929543bf5624ad5c31b20fda83d0d72576ac2591
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-07-07 17:32:35 +0300

    sorting questions by specific fields

--- expressjs/question-answers_rest_api_project/controllers/question.js
+++ expressjs/question-answers_rest_api_project/controllers/question.js
@@ -57,6 +57,18 @@ const getAllQuestions = asyncErrorWrapper(async (req, res, next) => {
     }
     query = query.skip(startIndex).limit(limit);
 
+    const sortKey= req.query.sortBy
+    if(sortKey == "most-answered"){
+        query = query.sort("-answerCount -createdAt")
+    }
+    if(sortKey == "most-liked"){
+        query = query.sort("-likeCount")
+    }
+    else{
+        query = query.sort("-createdAt")
+    }
+
+
     const questions = await query;
 
     return res.status(200).json({
