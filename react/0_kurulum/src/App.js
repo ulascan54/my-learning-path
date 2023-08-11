81aa6bfa52eef08a3015c10d25e482b1b3138cb1
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-11 15:46:36 +0300

    react initial

---
+++ react/0_kurulum/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png" alt="" />
+      <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default App
