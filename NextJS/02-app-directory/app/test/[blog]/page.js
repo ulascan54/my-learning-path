e1106122680c4c0773631c1a3a8ea7e377c202bb
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-29 17:31:18 +0300

    not found component

--- NextJS/02-app-directory/app/test/[blog]/page.js
+++ NextJS/02-app-directory/app/test/[blog]/page.js
@@ -1,8 +1,12 @@
 import React from "react"
+import { notFound } from "next/navigation"
 
 export default function BlogPage({ searchParams }) {
   if (searchParams.error === "true") {
     throw new Error("this is an error")
+  }
+  if (searchParams.test === "true") {
+    notFound()
   }
   return <div>BlogPage</div>
 }
--- NextJS/02-app-directory/app/test/page.js
+++ NextJS/02-app-directory/app/test/page.js
@@ -1,9 +1,9 @@
-async function delay(ms) {
-  return await new Promise((resolve) => setTimeout(resolve, ms))
-}
+// async function delay(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms))
+// }
 
 async function TestRoute() {
-  await delay(3000)
+  // await delay(3000)
   return <div>TestRoute</div>
 }
 export default TestRoute
