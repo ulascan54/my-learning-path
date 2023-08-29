e25f2b4dd46603f47d99c8999466e07d1aa83fdd
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-29 17:27:00 +0300

    error component

--- NextJS/02-app-directory/app/test/[blog]/page.js
+++ NextJS/02-app-directory/app/test/[blog]/page.js
@@ -1,5 +1,8 @@
 import React from "react"
 
-export default function BlogPage() {
+export default function BlogPage({ searchParams }) {
+  if (searchParams.error === "true") {
+    throw new Error("this is an error")
+  }
   return <div>BlogPage</div>
 }
