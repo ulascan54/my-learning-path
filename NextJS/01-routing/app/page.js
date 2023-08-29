4e3fea65de8cc5f9e8022733f424e68778fdeae5
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-29 12:13:57 +0300

    link component

--- NextJS/01-routing/app/page.js
+++ NextJS/01-routing/app/page.js
@@ -1,3 +1,17 @@
+import Link from "next/link"
+
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <div>
+      <Link href="/about">hakkımızda</Link>
+      <Link href="/docs/post-1">post1</Link>
+      <Link href="/docs/post-2">post2</Link>
+      <Link
+        className="tests "
+        href={{ pathname: "/about", query: { name: "Next.js", surname: "ff" } }}
+      >
+        hakkımızda obje
+      </Link>
+    </div>
+  )
 }
