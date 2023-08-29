66043af6193d86b4b2ad010b84df8c6bd465414b
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-29 09:55:52 +0300

    optional catch our house

--- NextJS/01-routing/app/[...blog]/page.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function Blog({ params }) {
-  return <div>Blog sayfasi: {params.blog}</div>
-}
---
+++ NextJS/01-routing/app/docs/[[...post]]/page.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function PostPage({ params }) {
+  return <div>PostPage: {params.post}</div>
+}
