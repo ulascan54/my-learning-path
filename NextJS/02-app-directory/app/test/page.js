095716c86c9563a65ade7ad36583c0acdcf644b2
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-29 17:15:03 +0300

    loading component

--- NextJS/02-app-directory/app/test/page.js
+++ NextJS/02-app-directory/app/test/page.js
@@ -1,5 +1,9 @@
-import React from "react"
+async function delay(ms) {
+  return await new Promise((resolve) => setTimeout(resolve, ms))
+}
 
-export default function TestRoute() {
+async function TestRoute() {
+  await delay(3000)
   return <div>TestRoute</div>
 }
+export default TestRoute
