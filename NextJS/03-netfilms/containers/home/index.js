8d3790a396a5614cb8b370a62030a08f2286a73b
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-08-30 12:49:40 +0300

    movies section

--- NextJS/03-netfilms/containers/home/index.js
+++ NextJS/03-netfilms/containers/home/index.js
@@ -3,12 +3,21 @@ import Movies from "@/mocks/movies.json"
 import Genres from "@/mocks/genres.json"
 import FeaturedMovie from "@/components/featured-movie"
 import Categories from "@/components/categories"
+import MoviesSection from "@/components/movies-section"
 
 function HomeContainer() {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
+      <MoviesSection
+        title="Populer Fılms"
+        movies={Movies.results.slice(1, 7)}
+      />
+      <MoviesSection
+        title="Your Favortıes"
+        movies={Movies.results.slice(7, 13)}
+      />
     </div>
   )
 }
