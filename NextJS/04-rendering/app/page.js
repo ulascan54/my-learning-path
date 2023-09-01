2a1e234bc4a262d7c22517cc078b93f2451c299e
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-01 17:04:24 +0300

    client side rendering

--- NextJS/04-rendering/app/page.js
+++ NextJS/04-rendering/app/page.js
@@ -1,13 +1,18 @@
-import React from "react"
+"use client"
+import React, { useEffect } from "react"
 import Button from "./button"
-import ButtonC from "@/components/button"
+// import ButtonC from "@/components/button"
 
-export default function Home() {
+function Home() {
+  useEffect(() => {
+    console.log("use effect")
+  }, [])
   console.log("HomePage")
   return (
     <div>
       <Button />
-      <ButtonC />
+      {/* <ButtonC /> */}
     </div>
   )
 }
+export default Home
