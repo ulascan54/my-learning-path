4d96f72e71337d361d18de58c4f6ce42590c8efc
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-06 17:54:33 +0300

    course modules

--- expressjs/question-answers_rest_api_project/client/src/App.js
+++ expressjs/question-answers_rest_api_project/client/src/App.js
@@ -1,17 +1,65 @@
 import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-function App() {
+import { Breadcrumb, Layout, Menu, theme } from "antd"
+const { Header, Content, Footer } = Layout
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken()
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <Navbar />
-          <div id="content">
-            <Routes>Route</Routes>
-          </div>
+    <Layout className="layout">
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={() => (
+            <div>
+              <div className="">selam</div>
+              <div className="">selam</div>
+              <div className="">selam</div>
+              <div className="">selam</div>
+            </div>
+          )}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 50px",
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          Content
         </div>
-      </Router>
-    </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
   )
 }
 
