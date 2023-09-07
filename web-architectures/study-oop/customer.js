5f0a1f42083b91c4725b6af439b2f0a5a9d9d66d
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-07 22:32:47 +0300

    object reference

--- web-architectures/study-oop/customer.js
+++ web-architectures/study-oop/customer.js
@@ -1,13 +1,19 @@
 Customer = function (identity, fullname, email) {
+  // this -> object reference
+  var self = this // reference değişkende saklarız
   // before es6
   //constructor function
   this.identity = identity
   this.fullname = fullname
   this.email = email
   this.sayHello = function () {
-    console.log("hello " + this.fullname)
+    console.log("hello " + self.fullname)
   }
 }
 
 jack = new Customer("11111", "jack baure", "jack@gmail.com")
-jack.sayHello()
+jack.sayHello() // sayhello , jack-> this
+setInterval(jack.sayHello, 3000) //window
+//jack.sayhello çalıştığında window -> this olur
+window.fullname = "kate ousten"
+setInterval(jack.sayHello, 3000) //window
