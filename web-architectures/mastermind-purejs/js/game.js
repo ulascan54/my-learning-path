113a79e29f3448e590debdcd0bd7c0941cc03682
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-09 16:37:02 +0300

    implement initialization  and play logic

--- web-architectures/mastermind-purejs/js/game.js
+++ web-architectures/mastermind-purejs/js/game.js
@@ -1,3 +1,10 @@
+class Move {
+  constructor(guess, message) {
+    this.guess = guess
+    this.message = message
+  }
+}
+
 class GameViewModel {
   constructor() {
     // #region fields
@@ -23,7 +30,57 @@ class GameViewModel {
     return digits.reduce((number, digit) => 10 * number + digit, 0)
   }
 
+  play(guess = 123) {
+    guess = Number(guess) // 549
+    this.tries++
+    if (guess === this.secret) {
+      this.gameLevel++
+      // TODO: check whether game is over!
+      this.init()
+    } else {
+      // TODO: check whether tries is over 10
+      let message = this.createMessage(this.secret, guess)
+      this.moves.push(new Move(guess, message))
+    }
+  }
+
   createRandomDigit = (max, min) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  init = () => {
+    this.moves = []
+    this.secret = this.createSecret()
+    this.tries = 0
+  }
+
+  createMessage = (secret, guess) => {
+    let stringSecret = secret.toString()
+    let stringGuess = guess.toString()
+    let perfectMatch = 0
+    let partialMatch = 0
+    for (let i = 0; i < stringSecret.length; i++) {
+      let s = stringSecret.charAt(i)
+      for (let j = 0; j < stringGuess.length; j++) {
+        let g = stringGuess.charAt(i)
+        if (s === g) {
+          if (i === j) {
+            perfectMatch++
+          } else {
+            partialMatch++
+          }
+        }
+      }
+    }
+    return this.createMessageFromMatches(perfectMatch, partialMatch)
+  }
+
+  createMessageFromMatches = (perfectMatch, partialMatch) => {
+    if (perfectMatch === 0 && partialMatch === 0) return "No match :("
+    let message = ""
+    if (partialMatch > 0) message = `-${partialMatch}`
+    if (perfectMatch > 0) message += `+${perfectMatch}`
+
+    return message
   }
 }
