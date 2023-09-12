fe676151c71e6700963c9914913661d1c2bbcf15
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-12 14:04:43 +0300

    jquery exercise1

---
+++ web-architectures/study-jquery/js/exercise02.js
@@ -0,0 +1,12 @@
+// window.onload = () => {
+//   //application
+// }
+
+jQuery(document).ready(() => {
+  //application
+  alert("first ready Function!")
+})
+
+$(document).ready(() => {
+  alert("Second ready Function!")
+})
