e59873a3a71d0385c6ca1ac4759cfb3459b66d00
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-12 14:19:40 +0300

    jquery selectors

--- web-architectures/study-jquery/js/exercise02.js
+++ web-architectures/study-jquery/js/exercise02.js
@@ -1,12 +1,6 @@
-// window.onload = () => {
-//   //application
-// }
-
-jQuery(document).ready(() => {
-  //application
-  alert("first ready Function!")
-})
-
-$(document).ready(() => {
-  alert("Second ready Function!")
+$("document").ready(() => {
+  $("#movies tbody > tr").css("border", "2px solid black")
+  $("#movies tbody > tr:odd").css("background-color", "blue")
+  $("#movies tbody > tr:even").css("background-color", "red")
+  $(".panel-heading").css("border", "4px solid green")
 })
