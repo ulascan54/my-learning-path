3219c0394d7bd3274516c8c884c618685ef1a045
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-14 18:57:14 +0300

    react twitter clone initial

---
+++ react/react-twitterclone/src/routes/index.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom"
+
+createBrowserRouter([
+  { path: "/", element: "anasayfa componenti" },
+  { path: "/explore", element: "explore componenti" },
+  { path: "/notificatilons", element: "bildirimler componenti" },
+])
