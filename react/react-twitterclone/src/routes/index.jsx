fb12c9bdb4821b23ba743c6712c281b256d89025
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-14 19:18:05 +0300

    absolute path & router component

--- react/react-twitterclone/src/routes/index.jsx
+++ react/react-twitterclone/src/routes/index.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom"
-
+import Home from "@/pages/home"
+import Explore from "@/pages/expolore"
+import Notifications from "@/pages/notifications"
 const routes = createBrowserRouter([
-  { path: "/", element: "anasayfa componenti" },
-  { path: "/explore", element: "explore componenti" },
-  { path: "/notificatilons", element: "bildirimler componenti" },
+  { path: "/", element: <Home /> },
+  { path: "/explore", element: <Explore /> },
+  { path: "/notifications", element: <Notifications /> },
 ])
 
 export default routes
