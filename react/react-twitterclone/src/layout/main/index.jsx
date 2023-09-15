7af2984326f9d97f5c2b63471ab9d82913dc8d54
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-15 19:04:46 +0300

    layout config complate

--- react/react-twitterclone/src/layout/main/index.jsx
+++ react/react-twitterclone/src/layout/main/index.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "../sidebar"
+import Rightbar from "../rightbar"
 
 export default function MainLayout() {
   return (
     <div className="w-[1265px] mx-auto flex">
       <Sidebar />
-      <main className="flex-1 border-x border-[#2f3336]">
-        <Outlet />
+      <main className="flex-1 flex gap-[30px]">
+        <main className="flex-1 max-w-[600px] border-x border-[#2f3336]">
+          <Outlet />
+        </main>
+        <Rightbar />
       </main>
     </div>
   )
---
+++ react/react-twitterclone/src/layout/rightbar/index.jsx
@@ -0,0 +1,3 @@
+export default function Rightbar() {
+  return <aside className="w-[350px] mr-2.5">Rightbar</aside>
+}
