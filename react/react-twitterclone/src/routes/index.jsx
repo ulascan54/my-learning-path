f655d549adb6f19bb67e9c5d62e068d5395982ff
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-15 16:40:45 +0300

    notfound component

--- react/react-twitterclone/src/routes/index.jsx
+++ react/react-twitterclone/src/routes/index.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom"
 import Home from "@/pages/home"
 import Explore from "@/pages/expolore"
 import Notifications from "@/pages/notifications"
+import NotFound from "@/pages/not-found"
 const routes = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/explore", element: <Explore /> },
   { path: "/notifications", element: <Notifications /> },
+  { path: "*", element: <NotFound /> },
 ])
 
 export default routes
