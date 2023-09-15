71ae0049352735999adfb73764051f44fb15fd91
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-15 19:01:18 +0300

    right & side barsg

--- react/react-twitterclone/src/layout/main/index.jsx
+++ react/react-twitterclone/src/layout/main/index.jsx
@@ -1,3 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Sidebar from "../sidebar"
+
 export default function MainLayout() {
-  return <div>mainlayout</div>
+  return (
+    <div className="w-[1265px] mx-auto flex">
+      <Sidebar />
+      <main className="flex-1 border-x border-[#2f3336]">
+        <Outlet />
+      </main>
+    </div>
+  )
 }
---
+++ react/react-twitterclone/src/layout/sidebar/index.jsx
@@ -0,0 +1,3 @@
+export default function Sidebar() {
+  return <aside className="w-[275px]">Sidebar</aside>
+}
