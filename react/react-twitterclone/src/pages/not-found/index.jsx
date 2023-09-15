6a39af27560bb311e82ae80087b970272d9236c1
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-09-15 16:58:32 +0300

    created main layout

--- react/react-twitterclone/src/pages/not-found/index.jsx
+++ react/react-twitterclone/src/pages/not-found/index.jsx
@@ -1,3 +1,7 @@
 export default function NotFound() {
-  return <div>NotFound</div>
+  return (
+    <div className="bg-red-200 text-red-600 flex items-center justify-center">
+      NotFound
+    </div>
+  )
 }
--- react/react-twitterclone/src/routes/index.jsx
+++ react/react-twitterclone/src/routes/index.jsx
@@ -3,10 +3,17 @@ import Home from "@/pages/home"
 import Explore from "@/pages/expolore"
 import Notifications from "@/pages/notifications"
 import NotFound from "@/pages/not-found"
+import MainLayout from "@/layout/main"
 const routes = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/explore", element: <Explore /> },
-  { path: "/notifications", element: <Notifications /> },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "explore", element: <Explore /> },
+      { path: "notifications", element: <Notifications /> },
+    ],
+  },
   { path: "*", element: <NotFound /> },
 ])
 
