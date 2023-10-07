2ded8a702580eec29e7169e2756beeb4a5683420
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-10-07 10:11:09 +0300

    logical operators cpp

---
+++ c++/7-logical-operators.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /*
+    or   &&
+    and  ||
+    not  !
+     */
+    string sys_name = "ulas";
+    string sys_password = "123";
+
+    string name, password;
+
+    cout << "kullanici adinizi giriniz" << endl;
+    cin >> name;
+
+    cout << "şifrenizi giriniz" << endl;
+    cin >> password;
+
+    if (sys_password != password && name != sys_name)
+    {
+        cout << "hatalı giriş yapıldı" << endl;
+    }
+    else if (sys_password != password && name == sys_name)
+    {
+        cout << "şifre hatalı" << endl;
+    }
+    else if (sys_password == password && name != sys_name)
+    {
+        cout << "kullanıcı adı hatalı" << endl;
+    }
+    else
+    {
+        cout << "hoşgeldiniz...." << endl;
+    }
+
+    return 0;
+}
