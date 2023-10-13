5e0069a32bbb47f081d5db27b6d5ea68bebfb188
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-10-13 09:15:34 +0300

    circumference and area

---
+++ c/9-circle-circumference-program.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    const double PI = 3.14159;
+    double radius;
+    double circumference;
+    double area;
+
+    printf("\nEnter the radius of a circle: ");
+    scanf("%lf", &radius);
+
+    circumference = 2 * PI * radius;
+    area = PI * radius * radius;
+
+    printf("\ncircumference: %lf", circumference);
+    printf("\narea: %lf", area);
+
+    return 0;
+}
