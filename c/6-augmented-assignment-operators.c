497910a86284b3c21715fe535bda45a40446579f
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-10-13 08:44:44 +0300

    augmented assigment operators

---
+++ c/6-augmented-assignment-operators.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int x = 10;
+    x = x + 2;
+    x += 2;
+
+    x = x - 1;
+    x -= 1;
+    x--;
+
+    x *= 4;
+
+    x /= 4;
+
+    x %= 2;
+
+    printf("%f", z);
+
+    return 0;
+}
