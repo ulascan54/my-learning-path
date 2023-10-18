0b2fe23c8e5c2dc36ebed05cb6791db4462a8af4
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-10-18 22:37:04 +0300

    recursive example

--- c/pre-exam/recursive/6-reversestring.c
+++ c/pre-exam/recursive/6-reversestring.c
@@ -4,11 +4,45 @@ author: ulaşcan
 question:
 Bir C programı yazın, bu program özyinelemeli bir işlev kullanarak bir metni ters çevirsin. Kullanıcıdan bir metin dizisi girmesini isteyin ve bu metni ters çevirmek için özyinelemeli bir işlev kullanın.
 */
-
 #include <stdio.h>
+#include <string.h>
+
+int findlen(char *s)
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
+void findrev(char *str, int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
+    else
+    {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        findrev(str, start + 1, end - 1);
+    }
+}
 
 int main()
 {
+    char string[30];
+
+    printf("Bir metin girin: ");
+    scanf("%s", string);
+
+    int len = findlen(string);
+    findrev(string, 0, len - 1);
+
+    printf("Ters çevrilmiş metin: %s\n", string);
 
     return 0;
 }
