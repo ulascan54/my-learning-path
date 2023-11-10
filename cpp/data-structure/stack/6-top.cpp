b10edf4a7dafe6f7d5b10c2a5d1193960996ba8e
Ulaş Can Demirbağ <39746408+ulascan54@users.noreply.github.com>
2023-11-10 21:50:18 +0300

    stack - top

---
+++ cpp/data-structure/stack/6-top.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class StackNode
+{
+public:
+    int data;
+    StackNode *next;
+    StackNode(const int &data = 0, StackNode *next = NULL) : data(data), next(next){};
+};
+
+class Stack
+{
+    StackNode *root;
+
+public:
+    Stack() : root(NULL){};
+    StackNode *begin() const
+    {
+        return root;
+    };
+    StackNode *end() const
+    {
+        return NULL;
+    }
+    bool isEmpty() const
+    {
+        // return root == NULL;
+        return begin() == end();
+    }
+    void push(const int &value)
+    {
+        StackNode *tmp = new StackNode(value, root);
+        root = tmp;
+    }
+    void display() const
+    {
+        if (isEmpty())
+            return;
+        StackNode *tmp = begin();
+        while (tmp != end())
+        {
+            cout << tmp->data << " ";
+            tmp = tmp->next;
+        }
+        cout << endl;
+    }
+    void pop()
+    {
+        if (isEmpty())
+            return;
+        StackNode *tmp = root;
+        root = root->next;
+        delete tmp;
+    }
+
+    int top() const
+    {
+        if (isEmpty())
+        {
+            throw runtime_error("error");
+        }
+
+        return begin()->data;
+    }
+};
+
+int main()
+{
+    Stack s;
+    cout << s.isEmpty() << endl;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout << s.isEmpty() << endl;
+    s.display();
+    s.pop();
+    s.display();
+    cout << s.top() << endl;
+    return 0;
+}
